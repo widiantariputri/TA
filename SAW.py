c06b1c064f0b0d10a4a667032204a49bcbf1356a
kalehub <=>
2021-04-21 22:53:51 +0800

    refactoring using OOP

--- SAW.py
+++ SAW.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# File              : main.py
+# File              : SAW.py
 # Author            : Teguh Satya <teguhsatyadhr@gmail.com>
 # Date              : 21.04.2021
 # Last Modified Date: 21.04.2021
 # Last Modified By  : Teguh Satya <teguhsatyadhr@gmail.com>
-import numpy as np
-import math
-from SAW import *
-from ANP import *
 
 '''
-
-This is the driver code
-A main portal to access all of the classes.
+This file only include SAW
+process.
 
 
 '''
 
-def main():
-    anp = ANP()
-    saw = SAW()
+class SAW:
+    def __init__(self):
+        print('SAW here!')
+    
 
 
 
 
-if __name__ == '__main__':
-    main()

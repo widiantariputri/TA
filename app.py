03cf3dda4b036aa9835a39965ef23fb3dfe8f542
teguhsatyadharma <hiteguhsatya@gmail.com>
2021-09-03 14:38:47 +0800

    KARYA AGUNG PART 02

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/bobot")
+@app.route("/bobot", methods=['GET', 'POST'])
 def bot_kepentingan():
 
     dataset_name = session.get('dataset')
@@ -36,10 +36,46 @@ def bot_kepentingan():
     # kriteria yg tersedia
     kriteria = ['Measurable', 'Capital', 'Alternative']
 
-    # subkriteria didapat dari dataset
+    if request.method == 'POST':
+        print('POST!')
 
-    return render_template("bobot.html", context={'kriteria': kriteria,
-                                                  'subkriteria': subkriteria})
+        # Getting the kriteria and sub kriteria
+        # range input
+        kriteria_input_value = request.form.getlist('kriteria-list[]')
+        subkriteria_input_value = request.form.getlist('sub-list[]')
+
+        # Getting the dropdown bobot input
+        bobot_from = request.form.getlist('bobot_kriteria_from[]')
+        bobot_to = request.form.getlist('bobot_kriteria_to[]')
+
+        # Getting the dropdown sub input
+        sub_from = request.form.getlist('sub_from[]')
+        sub_to = request.form.getlist('sub_to[]')
+
+        # Turning it into dictionary (object) so it can be easily
+        # stored and accessed via session
+        bobot_form = dict()
+        bobot_form['from'] = bobot_from
+        bobot_form['to'] = bobot_to
+
+        print(bobot_form)
+
+        sub_form = dict()
+        sub_form['from'] = sub_from
+        sub_form['to'] = sub_to
+
+        session['bobot_form'] = bobot_form
+        session['sub_form'] = sub_form
+        session['kriteria_range'] = kriteria_input_value
+        session['subkriteria_range'] = subkriteria_input_value
+
+        return redirect(url_for('hasil_pencarian'))
+    else:
+        # subkriteria didapat dari dataset
+
+        return render_template("bobot.html",
+                               context={'kriteria': kriteria,
+                                        'subkriteria': subkriteria})
 
 
 @ app.route("/upload", methods=['GET', 'POST'])
@@ -79,8 +115,9 @@ def allowed_file(filename):
         return False
 
 
-@ app.route("/hasil/<filename>", methods=['GET', 'POST'])
-def hasil_pencarian(filename):
+@ app.route("/hasil/", methods=['GET', 'POST'])
+def hasil_pencarian():
+    '''
     DDIR = 'UPLOADS/'+filename
     SURVEY_DATA = 'DATASET/ANOTHER_RESPONDEN.csv'
 
@@ -115,6 +152,9 @@ def hasil_pencarian(filename):
     saw = SAW()
     hasil_saw = saw.get_hasil()
     return render_template('result.html', hasil=hasil_saw)
+    '''
+
+    return render_template('result.html')
 
 
 if __name__ == '__main__':
